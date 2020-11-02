1c6ab23e5a435d0bce2de809d4c70c8fcc175d80
Ntinos92 <Konstantinosdim92@gmail.com>
2020-11-02 15:36:55 +0200

    Added Basket Model

--- MyShopDemo/MyShopDemo.DataAccess.SQL/DataContext.cs
+++ MyShopDemo/MyShopDemo.DataAccess.SQL/DataContext.cs
@@ -16,5 +16,7 @@ namespace MyShopDemo.DataAccess.SQL
 
     public DbSet<Product> Products { get; set; }
     public DbSet<ProductCategory> ProductCategories { get; set; }
+    public DbSet<Basket> Baskets { get; set; }
+    public DbSet<BasketItem> BasketsItems { get; set; }
     }
 }
